48dbf07246a90fd63ac63f6d65447a3c50479f1d
Bianca Botezatu <botezatubianca20@gmail.com>
2019-12-13 23:23:57 +0200

    firebase config

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var faker = require("faker");
+var path = require("path"); 
+var firebase = require("firebase");
+var cors = require ("cors"); 
+var bodyPasrser = require("body-parser");
+
+app.use(bodyPasrser.json());
+app.use(bodyPasrser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(cors());
+
+
+
+
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACpltbqxrDKDoqUf-IO0N7ZF5Eps-FWyU",
+    authDomain: "ticproject-5936a.firebaseapp.com",
+    databaseURL: "https://ticproject-5936a.firebaseio.com",
+    projectId: "ticproject-5936a",
+    storageBucket: "ticproject-5936a.appspot.com",
+    messagingSenderId: "764982610729",
+    appId: "1:764982610729:web:c87175130343502adb0538",
+    measurementId: "G-8X38VKE306"
+  };
