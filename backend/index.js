6fb68e3ae553c2a8c2bf0c0f251968c587d5f916
Bianca Botezatu <botezatubianca20@gmail.com>
2019-12-23 22:48:44 +0200

    faker doctors and patients

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ var path = require("path");
 var firebase = require("firebase");
 var cors = require ("cors"); 
 var bodyPasrser = require("body-parser");
+var faker = require("faker");
 
 app.use(bodyPasrser.json());
 app.use(bodyPasrser.urlencoded({extended: true}));
@@ -19,6 +20,64 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://ticproject-5936a.firebaseio.com"
 });
+
+
+var db = admin.database();
+var doctorsTable = db.ref("doctors");
+var patientsTable = db.ref("patients");
+
+var counter = 0;
+var totalNumber = 0;
+
+app.get('/addData/:number', function(req, res){
+  totalNumber = totalNumber + parseInt(req.params.number);
+	for (counter; counter < totalNumber; counter++) {
+    var randomIdDoctor = 0 + Math.floor(Math.random()*totalNumber);
+    var randomFirstNameDoctor =  faker.name.firstName();
+    var randomLastNameDoctor =  faker.name.lastName();
+    var randomAgeDoctor = 18 + Math.floor(Math.random() * 50);
+    var randomEmailDoctor = faker.internet.email();
+    var randomPhoneNumberDoctor = faker.phone.phoneNumber();
+    var randomSpecializationDoctor = faker.lorem.word();
+
+    var doctor = doctorsTable.push({ 
+      id: randomIdDoctor, 
+      first_name: randomFirstNameDoctor,
+      last_name: randomLastNameDoctor,
+      age: randomAgeDoctor,	
+      email: randomEmailDoctor,
+      phone_number: randomPhoneNumberDoctor,
+      specialization: randomSpecializationDoctor
+    });
+    console.log(doctor.key); 
+
+    var randomIdPatient = 0 + Math.floor(Math.random()*totalNumber);
+    var randomFirstNamePatient =  faker.name.firstName();
+    var randomLastNamePatient =  faker.name.lastName();
+    var randomAgePatient = 8 + Math.floor(Math.random() * 70);
+    var randomEmailPatient = faker.internet.email();
+    var randomPhoneNumberPatient = faker.phone.phoneNumber();
+
+    var patient = patientsTable.push({
+      id: randomIdPatient,
+      first_name: randomFirstNamePatient,
+      last_name: randomLastNamePatient,
+      age: randomAgePatient,	
+      email: randomEmailPatient,
+      phone_number: randomPhoneNumberPatient,
+    })
+    console.log(patient.key); 
+
+    // var appointment = appointmentsTable.push({
+    //   id: randomIdAppointment,
+    // })
+  }
+  res.send("db filled with data");
+
+ 
+
+})
+
 
 
 
