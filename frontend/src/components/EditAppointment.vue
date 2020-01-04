858263fb646c300029e3fb38d4013060e012b5e9
Bianca Botezatu <botezatubianca20@gmail.com>
2020-01-04 16:29:05 +0200

    add and edit appointments done with patients and doctors names in b-form-select

--- frontend/src/components/EditAppointment.vue
+++ frontend/src/components/EditAppointment.vue
@@ -16,14 +16,14 @@
   <div class="form-group">
       <label >Pacient</label>
     <div>
-    <b-form-select v-model="appointment.patient_id" :options="patients"></b-form-select>
+    <b-form-select v-model="selectedPatient" :options="patientsNames"></b-form-select>
   </div>
   </div>
  
   <div class="form-group">
       <label >Doctor</label>
     <div>
-    <b-form-select v-model="appointment.doctor_id" :options="doctors"></b-form-select>
+    <b-form-select v-model="selectedDoctor" :options="doctorsNames"></b-form-select>
   </div>
   </div>
 
@@ -51,8 +51,12 @@ import datePicker from 'vue-bootstrap-datetimepicker';
           appointment: this.$route.params.appointment,
           patients: [],
           doctors: [],
-          selectedPatient: null,
-          selectedDoctor: null,
+          doctorsValues: [],
+          patientsValues: [], 
+          patientsNames: [],
+          doctorsNames: [],
+          selectedPatient: this.$route.params.appointment.patient_id,
+          selectedDoctor: this.$route.params.appointment.doctor_id,
           hour: "",
           last_name: "",
           age: "",
@@ -68,9 +72,19 @@ import datePicker from 'vue-bootstrap-datetimepicker';
     created: function() {
       this.axios.get('http://localhost:3000/patients').then(response => {
         this.patients = response.data;
+        this.patientsValues = Object.values(this.patients);
+        var patientsKeys = Object.keys(this.patients);
+        for(var i=0; i<patientsKeys.length; i++){
+          this.patientsNames.push({value: patientsKeys[i], text: this.patientsValues[i].last_name + ' ' + this.patientsValues[i].first_name})
+        }
       });
       this.axios.get('http://localhost:3000/doctors').then(response => {
         this.doctors = response.data;
+        this.doctorsValues = Object.values(this.doctors);
+        var doctorsKeys = Object.keys(this.doctors);
+        for(var i=0; i<doctorsKeys.length; i++){
+          this.doctorsNames.push({value: doctorsKeys[i], text: this.doctorsValues[i].last_name + ' ' + this.doctorsValues[i].first_name})
+        }
       });
     },
 
@@ -79,8 +93,8 @@ import datePicker from 'vue-bootstrap-datetimepicker';
         this.axios.put('http://localhost:3000/appointments/' + this.key, {
             appointment_date: new Date(this.date).getTime(),
             hour: this.appointment.hour,
-            patient_id: this.appointment.patient_id,
-            doctor_id: this.appointment.doctor_id
+            patient_id: this.selectedPatient,
+            doctor_id: this.selectedDoctor
         })
         .then(function(){ 
             alert("The appointment has been updated!");
--- frontend/src/components/NewAppointment.vue
+++ frontend/src/components/NewAppointment.vue
@@ -16,14 +16,14 @@
   <div class="form-group">
       <label >Pacient</label>
     <div>
-    <b-form-select v-model="selectedPatient" :options="patients"></b-form-select>
+    <b-form-select v-model="selectedPatient" :options="patientsNames"></b-form-select>
   </div>
   </div>
  
   <div class="form-group">
       <label >Doctor</label>
     <div>
-    <b-form-select v-model="selectedDoctor" :options="doctors"></b-form-select>
+    <b-form-select v-model="selectedDoctor" :options="doctorsNames"></b-form-select>
   </div>
   </div>
 
@@ -49,6 +49,10 @@ import datePicker from 'vue-bootstrap-datetimepicker';
       return {
           patients: [],
           doctors: [],
+          doctorsValues: [],
+          patientsValues: [], 
+          patientsNames: [],
+          doctorsNames: [],
           selectedPatient: null,
           selectedDoctor: null,
           hour: "",
@@ -66,9 +70,19 @@ import datePicker from 'vue-bootstrap-datetimepicker';
     created: function() {
       this.axios.get('http://localhost:3000/patients').then(response => {
         this.patients = response.data;
+        this.patientsValues = Object.values(this.patients);
+        var patientsKeys = Object.keys(this.patients);
+        for(var i=0; i<patientsKeys.length; i++){
+          this.patientsNames.push({value: patientsKeys[i], text: this.patientsValues[i].last_name + ' ' + this.patientsValues[i].first_name})
+        }
       });
       this.axios.get('http://localhost:3000/doctors').then(response => {
         this.doctors = response.data;
+        this.doctorsValues = Object.values(this.doctors);
+        var doctorsKeys = Object.keys(this.doctors);
+        for(var i=0; i<doctorsKeys.length; i++){
+          this.doctorsNames.push({value: doctorsKeys[i], text: this.doctorsValues[i].last_name + ' ' + this.doctorsValues[i].first_name})
+        }
       });
     },
 
